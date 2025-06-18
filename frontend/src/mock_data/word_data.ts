d5d39905b5000e419218c79c731c744b1bab9a69
Saher <saher.ziauddin@dal.ca>
2025-06-18 17:47:00 -0300

    Added mock data

---
+++ frontend/src/mock_data/word_data.ts
@@ -0,0 +1,13 @@
+import { Words } from "../types/Word";
+
+  const wordInfo: Words = {
+    'The': { word: "The", definition: 'Definite article', type: 'Article' },
+    'quick': { word: "quick", definition: 'Moving fast or doing something in a short time', type: 'Adjective' },
+    'brown': { word: "brown", definition: 'A color between red and yellow', type: 'Adjective' },
+    'fox': { word: "fox", definition: 'A carnivorous mammal with a pointed snout', type: 'Noun' },
+    'jumps': { word: "jumps", definition: 'To push oneself off a surface', type: 'Verb' },
+    'over': { word: "over", definition: 'Extending directly upward from', type: 'Preposition' },
+    'the': { word: "the", definition: 'Definite article', type: 'Article' },
+    'lazy': { word: "lazy", definition: 'Unwilling to work or use energy', type: 'Adjective' },
+    'dog': { word: "dog", definition: 'A domesticated carnivorous mammal', type: 'Noun' },
+  };
