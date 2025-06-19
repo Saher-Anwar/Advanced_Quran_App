d5a705667180c4e41134e2dbaed27765cc189917
Saher <saher.ziauddin@dal.ca>
2025-06-19 04:21:30 -0300

    Mostly completed Home Screen

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ChapterScreen from './src/screens/ChapterScreen';
+import HomeScreen from './src/screens/HomeScreen';
 
 const App = () => {
   return (
-    <ChapterScreen/> 
+    <HomeScreen/>
   )
 }
 
