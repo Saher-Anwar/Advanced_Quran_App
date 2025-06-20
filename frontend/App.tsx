86e697532d8e23a9ba9d539db908e4e44a9eab1c
Saher <saher.ziauddin@dal.ca>
2025-06-20 01:27:48 -0300

    Fixed TailWind and NativeWind not working

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,17 +1,7 @@
-import React from "react";
-import { StyleSheet } from "react-native";
-import AppNavigator from "./src/navigation/AppNavigator";
+import { Text, View } from 'react-native';
+import './global.css';
+import AppNavigator from 'navigation/AppNavigator';
 
-const App = () => {
+export default function App() {
   return <AppNavigator />;
-};
-
-export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingVertical: 50,
-    backgroundColor: "gray",
-  },
-});
+}
